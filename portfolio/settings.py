5b8036c2ae36c1df3aa2d3d3d88ce04dc5d2ceef
facundovillalba38 <facundovillalba38@gmail.com>
2021-01-25 22:59:08 -0300

    host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'aei17nzm@r*ae5sgll)rtwxg3wb@mkhhxt@^4-z&-t6om1gg0n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
